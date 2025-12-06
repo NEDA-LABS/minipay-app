1424d36ec8cd9365b3f64b8e7e6d822c7d7f4919
barakacanbyte <barakajimmy@outlook.com>
2025-12-07 00:26:00 +0300

    fix build error

--- app/ramps/yellowcard/onramp/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnRampFlow } from '../components/OnRampFlow';
-
-export default function YellowCardOnRampPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <OnRampFlow />
-    </div>
-  );
-}
